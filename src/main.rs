8bdca9104806686c2f52ce9d79fb59c5333df82b
Joaquin Rosales <globojorro@gmail.com>
2017-11-06 20:09:36 -0600

    add command-line matching and evaluation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-//! Control interface for `benita` services
+//! Control interface for `benita` services.
 #![recursion_limit = "1024"]
 
 extern crate benita;
 extern crate chrono;
+extern crate clap;
 #[macro_use]
 extern crate error_chain;
 #[macro_use]
@@ -11,89 +12,71 @@ extern crate fern;
 
 /// Errors and their descriptions
 pub mod errors {
+    use super::logging;
     use benita;
+
     error_chain! {
+        errors {
+            InvalidArgs {
+                description ("invalid command-line arguments")
+            }
+        }
         links {
+            Logs(logging::Error, logging::ErrorKind);
             Benita(benita::errors::Error, benita::errors::ErrorKind);
         }
     }
 }
+pub mod logging;
 
-use errors::*;
+use self::errors::*;
+use logging::start_log;
 
-/// Configure and start logger.
-fn start_logger() -> Result<()> {
-    let base_config = fern::Dispatch::new();
+use clap::{App, AppSettings, ArgMatches, SubCommand};
 
-    let err_logger_file = fern::Dispatch::new()
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "{}[{}][{}] {}",
-                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
-                record.target(),
-                record.level(),
-                message
-            ))
-        })
-        .filter(|metadata| {
-                   // Reject messages with the `Info` log level, accept others.
-                   //
-                   // This could be useful for sending Error messages to stderr
-                   // and avoiding duplicate messages in stdout.
-                   metadata.level() != log::LogLevelFilter::Info
-        })
-        .chain(fern::log_file("benita-commander.err")
-               .chain_err(|| "could not setup error log file")?);
 
-    let err_logger = fern::Dispatch::new()
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "[{}][{}] {}",
-                record.target(),
-                record.level(),
-                message
-            ))
-        })
-        .level(log::LogLevelFilter::Trace)
-        .filter(|metadata| {
-                   // Reject messages with the `Info` log level, accept others.
-                   //
-                   // This could be useful for sending Error messages to stderr
-                   // and avoiding duplicate messages in stdout.
-                   metadata.level() != log::LogLevelFilter::Info
-        })
-        .chain(std::io::stderr());
-
-    let info_logger = fern::Dispatch::new()
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "{}[{}][{}] {}",
-                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
-                record.target(),
-                record.level(),
-                message
-            ))
-        })
-        .level(log::LogLevelFilter::Error)
-        .chain(std::io::stdout())
-        .chain(fern::log_file("benita-commander.log")
-            .chain_err(|| "failed to open log file")?);
-
-    let _start = base_config
-        .chain(err_logger_file)
-        .chain(err_logger)
-        .chain(info_logger)
-        .apply()
-        .chain_err(|| "Could not setup error logging")?;
+/// Execute the program from the given command-line arguments
+fn evaluate(args: &ArgMatches) ->Result<()> {
+    debug!("{:?}", args);
+    println!("Hello, world!");
     Ok(())
+}
+
+/// parse command line arguments, return the valid matches.
+fn parse_command_line<'a>() -> Result<ArgMatches<'a>> {
+    let cli_parser = App::new("benita-commander")
+            .settings(&[
+                // AppSettings::DisableHelpSubcommand,
+                AppSettings::SubcommandRequired,
+            ])
+            .subcommands(vec![
+                         SubCommand::with_name("conductivity"),
+                         SubCommand::with_name("ph"),
+                         SubCommand::with_name("temperature"),
+                         ]);
+    let matches = cli_parser
+        .get_matches_safe()
+        .chain_err(|| ErrorKind::InvalidArgs)?;
+    Ok(matches)
 }
 
 /// Main program. Starts logger, then evaluates args from stdin.
 fn run_code() -> Result<()> {
     // Initialize logging.
-    let _log = start_logger()?;
-    info!("Starting calibrated-service");
-    println!("Hello, world!");
+    let _log = start_log()?;
+
+    info!("benita-command starting");
+
+    // This runs the whole program. Everything has brought us to this point.
+    // Use it wisely.
+    {
+        // Parse the user input from the command-line.
+        let matched_args = parse_command_line()?;
+        let _run = evaluate(&matched_args)?;
+    }
+
+    warn!("About to stop calibrated-service");
+    info!("benita-command stopping");
     Ok(())
 }
 
