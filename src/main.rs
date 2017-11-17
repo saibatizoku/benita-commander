02b4dad56ae0a3e92ab452dfe7728a6313fa9b66
Joaquin Rosales <globojorro@gmail.com>
2017-11-16 22:55:42 -0600

    hookup CLI to REP and REQ instances for all sensors

--- src/main.rs
+++ src/main.rs
@@ -47,19 +47,177 @@ pub mod logging;
 pub mod readline;
 pub mod temperature;
 
+use std::thread;
+use std::time::Duration;
+
 use self::errors::*;
-use cli::{responder_subcommand, BenitaCommanderApp};
-use logging::start_log;
+use self::cli::BenitaCommanderApp;
+use self::conductivity::{ConductivityREP, ConductivityREQ};
+use self::logging::start_log;
+use self::ph::{PhREP, PhREQ};
+use self::readline::{CommanderReadline, SensorKind};
+use self::temperature::{TemperatureREP, TemperatureREQ};
 
-use clap::{App, AppSettings, ArgMatches, SubCommand};
-
+use benita::network::Endpoint;
+use clap::ArgMatches;
 
 /// Execute the program from the given command-line arguments
 fn evaluate(args: &ArgMatches) -> Result<()> {
     // This runs the whole program. Everything has brought us to this point.
     // Use it wisely.
-    debug!("{:?}", args);
-    println!("Hello, world!");
+    debug!(target: "benita-commander", "{:?}", args);
+
+    let _eval = match args.subcommand() {
+        ("", _args) => {
+            debug!("top-level readline");
+            println!("********************");
+            println!("* benita-commander *");
+            println!("********************");
+            CommanderReadline::Main.start(">> ", |s| s.to_string().to_uppercase())?
+        }
+        ("conductivity", _args) => {
+            debug!("conductivity readline");
+            match _args {
+                Some(m) => match m.subcommand() {
+                    ("rep", Some(rep_args)) => {
+                        let url = rep_args.value_of("URL").unwrap();
+                        let path = rep_args.value_of("I2C_PATH").unwrap();
+                        let addr = rep_args.value_of("I2C_ADDRESS").unwrap().parse().unwrap();
+
+                        let server = ConductivityREP::new(url, path, addr)?;
+                        loop {
+                            let req_str = &server.responder.recv()?;
+                            info!("REQ: {}", &req_str);
+                            let call: String = server.eval(&req_str)?;
+                            info!("REP: {}", &call);
+                            let _reply = &server.responder.send(call.as_bytes())?;
+
+                            thread::sleep(Duration::from_millis(400));
+                        }
+                    }
+                    ("req", Some(req_args)) => {
+                        let url = req_args.value_of("URL").unwrap();
+
+                        let requester = ConductivityREQ::new(url)?;
+
+                        let _exec = match req_args.values_of("cmds") {
+                            Some(cmds) => for cmd in cmds {
+                                let response = match requester.eval(cmd) {
+                                    Ok(parsed) => parsed,
+                                    _ => "command not in custom api".to_string(),
+                                };
+                                println!("REP: {}", response);
+                            },
+                            None => CommanderReadline::MainSensor(SensorKind::Conductivity)
+                                .start("conductivity>> ", |s| match requester.eval(s) {
+                                    Ok(response) => response,
+                                    _ => "command not in custom api".to_string(),
+                                })?,
+                        };
+                    }
+                    _ => println!("{}", m.usage()),
+                },
+                None => {}
+            }
+        }
+        ("ph", _args) => {
+            debug!(target: "benita-commander", "ph readline");
+            match _args {
+                Some(m) => match m.subcommand() {
+                    ("rep", Some(rep_args)) => {
+                        let url = rep_args.value_of("URL").unwrap();
+                        let path = rep_args.value_of("I2C_PATH").unwrap();
+                        let addr = rep_args.value_of("I2C_ADDRESS").unwrap().parse().unwrap();
+
+                        let server = PhREP::new(url, path, addr)?;
+                        loop {
+                            let req_str = &server.responder.recv()?;
+                            info!("REQ: {}", &req_str);
+                            let call: String = server.eval(&req_str)?;
+                            info!("REP: {}", &call);
+                            let _reply = &server.responder.send(call.as_bytes())?;
+
+                            thread::sleep(Duration::from_millis(400));
+                        }
+                    }
+                    // REQ Client that connects to the given URL, capable of
+                    // interpreting from a list of known commands.
+                    ("req", Some(req_args)) => {
+                        let url = req_args.value_of("URL").unwrap();
+
+                        let requester = PhREQ::new(url)?;
+
+                        let _exec = match req_args.values_of("cmds") {
+                            Some(cmds) => for cmd in cmds {
+                                let response = match requester.eval(cmd) {
+                                    Ok(parsed) => parsed,
+                                    _ => "command not in custom api".to_string(),
+                                };
+                                println!("REP: {}", response);
+                            },
+                            None => CommanderReadline::MainSensor(SensorKind::Ph).start(
+                                "ph>> ",
+                                |s| match requester.eval(s) {
+                                    Ok(response) => response,
+                                    _ => "command not in custom api".to_string(),
+                                },
+                            )?,
+                        };
+                    }
+                    _ => println!("{}", m.usage()),
+                },
+                None => {}
+            }
+        }
+        ("temperature", _args) => {
+            debug!(target: "benita-commander", "temperature readline");
+            match _args {
+                Some(m) => match m.subcommand() {
+                    ("rep", Some(rep_args)) => {
+                        let url = rep_args.value_of("URL").unwrap();
+                        let path = rep_args.value_of("I2C_PATH").unwrap();
+                        let addr = rep_args.value_of("I2C_ADDRESS").unwrap().parse().unwrap();
+
+                        let server = TemperatureREP::new(url, path, addr)?;
+                        loop {
+                            let req_str = &server.responder.recv()?;
+                            info!("REQ: {}", &req_str);
+                            let call: String = server.eval(&req_str)?;
+                            info!("REP: {}", &call);
+                            let _reply = &server.responder.send(call.as_bytes())?;
+
+                            thread::sleep(Duration::from_millis(400));
+                        }
+                    }
+                    ("req", Some(req_args)) => {
+                        let url = req_args.value_of("URL").unwrap();
+
+                        let requester = TemperatureREQ::new(url)?;
+
+                        let _exec =
+                            match req_args.values_of("cmds") {
+                                Some(cmds) => for cmd in cmds {
+                                    let response = match requester.eval(cmd) {
+                                        Ok(parsed) => parsed,
+                                        _ => "command not in custom api".to_string(),
+                                    };
+                                    println!("REP: {}", response);
+                                },
+                                None => CommanderReadline::MainSensor(SensorKind::Temperature)
+                                    .start("ph>> ", |s| match requester.eval(s) {
+                                        Ok(response) => response,
+                                        _ => "command not in custom api".to_string(),
+                                    })?,
+                            };
+                    }
+                    _ => println!("{}", m.usage()),
+                },
+                None => {}
+            }
+        }
+        _ => println!("{}", args.usage()),
+    };
+
     Ok(())
 }
 
